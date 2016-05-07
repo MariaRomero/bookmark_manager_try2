888f4f68dbd2c878b1350281d40bb37584ea93f6
MariaRomero <fantasysystems@gmail.com>
2016-05-07 22:24:13 +0100

    challenge 16 done

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,13 +3,9 @@ require './app/data_mapper_setup'
 class Tag
   include DataMapper::Resource
 
-  #has n, :links, :through => Resource
+  has n, :links, :through => Resource
 
   property :id,  Serial
   property :name, String
 
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
--- spec/feature/viewing_links_spec.rb
+++ spec/feature/viewing_links_spec.rb
@@ -1,7 +1,13 @@
 feature 'Viewing links' do
 
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.bbc.com', title: 'bbc', tags: [Tag.first_or_create(name: 'news')])
+    Link.create(url: 'http://www.bubblemaking.com', title: 'making bubbles', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubblesoftheworld.com', title: 'world bubbles', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
   scenario 'can see an existing link on the page' do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
     expect(page.status_code).to eq 200
 
@@ -9,4 +15,18 @@ feature 'Viewing links' do
       expect(page).to have_content('Makers Academy')
     end
   end
+
+  scenario 'can search for a link through tags' do
+    visit '/tags/bubbles'
+
+    expect(page.status_code).to eq(200)
+
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('bbc')
+      expect(page).to have_content('world bubble')
+      expect(page).to have_content('making bubbles')
+    end
+  end
+  
 end
